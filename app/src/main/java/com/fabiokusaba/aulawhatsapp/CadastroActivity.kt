b2f117ace1f6ad2d591ecea3daf3a4736d2ee8ab
Fabio Kusaba <fabinhokb44@gmail.com>
2023-12-27 20:07:50 -0300

    commit "adicionado tela de cadastro"

---
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/CadastroActivity.kt
@@ -0,0 +1,28 @@
+package com.fabiokusaba.aulawhatsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fabiokusaba.aulawhatsapp.databinding.ActivityCadastroBinding
+
+class CadastroActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        inicializarToolbar()
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Faça o seu cadastro"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+}
