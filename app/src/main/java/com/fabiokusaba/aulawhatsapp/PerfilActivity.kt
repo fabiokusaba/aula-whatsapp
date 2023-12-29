50d632ebaf5efa9077c85639b12016d665abc577
Fabio Kusaba <fabinhokb44@gmail.com>
2023-12-29 20:18:34 -0300

    commit "adicionado permissões para o uso do app"

--- app/src/main/java/com/fabiokusaba/aulawhatsapp/PerfilActivity.kt
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/PerfilActivity.kt
@@ -1,7 +1,13 @@
 package com.fabiokusaba.aulawhatsapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.fabiokusaba.aulawhatsapp.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
@@ -9,11 +15,50 @@ class PerfilActivity : AppCompatActivity() {
         ActivityPerfilBinding.inflate(layoutInflater)
     }
 
+    private var temPermissaoCamera = false
+    private var temPermissaoGaleria = false
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         inicializarToolbar()
+        solicitarPermissoes()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun solicitarPermissoes() {
+        //Verificar se o usuário já tem a permissão
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //Lista de permissões negadas
+        val listaPermissoesNegadas = mutableListOf<String>()
+
+        if (!temPermissaoCamera) {
+            listaPermissoesNegadas.add(Manifest.permission.CAMERA)
+        }
+
+        if (!temPermissaoGaleria) {
+            listaPermissoesNegadas.add(Manifest.permission.READ_MEDIA_IMAGES)
+        }
+
+        if (listaPermissoesNegadas.isNotEmpty()) {
+            //Solicitar múltiplas permissões
+            val gerenciadorPermissoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { permissoes ->
+                temPermissaoCamera = permissoes[Manifest.permission.CAMERA] ?: temPermissaoCamera
+                temPermissaoGaleria = permissoes[Manifest.permission.READ_MEDIA_IMAGES] ?: temPermissaoGaleria
+            }
+            gerenciadorPermissoes.launch(listaPermissoesNegadas.toTypedArray())
+        }
     }
 
     private fun inicializarToolbar() {
