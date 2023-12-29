e666bdb1166132d7af7d8473c91d61d9ef61dde5
Fabio Kusaba <fabinhokb44@gmail.com>
2023-12-29 20:55:35 -0300

    commit "adicionado atualização de perfil do usuário"

--- app/src/main/java/com/fabiokusaba/aulawhatsapp/PerfilActivity.kt
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/PerfilActivity.kt
@@ -2,13 +2,16 @@ package com.fabiokusaba.aulawhatsapp
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.fabiokusaba.aulawhatsapp.databinding.ActivityPerfilBinding
+import com.fabiokusaba.aulawhatsapp.utils.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 
 class PerfilActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -18,16 +21,107 @@ class PerfilActivity : AppCompatActivity() {
     private var temPermissaoCamera = false
     private var temPermissaoGaleria = false
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val storage by lazy {
+        FirebaseStorage.getInstance()
+    }
+
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    private val gerenciadorGaleria = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            binding.imagePerfil.setImageURI(uri)
+            uploadImagemStorage(uri)
+        } else {
+            exibirMensagem("Nenhuma imagem selecionada")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         inicializarToolbar()
         solicitarPermissoes()
+        inicializarEventosClique()
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun uploadImagemStorage(uri: Uri) {
+        //fotos -> usuarios -> idUsuario -> perfil.jpg
+        val idUsuario = firebaseAuth.currentUser?.uid
+
+        if (idUsuario != null) {
+            storage
+                .getReference("fotos")
+                .child("usuarios")
+                .child(idUsuario)
+                .child("perfil.jpg")
+                .putFile(uri)
+                .addOnSuccessListener { task ->
+                    exibirMensagem("Sucesso ao fazer o upload da imagem")
+
+                    task.metadata?.reference?.downloadUrl?.addOnSuccessListener { uri ->
+                        val dados = mapOf(
+                            "foto" to uri.toString()
+                        )
+
+                        atualizarDadosPerfil(idUsuario, dados)
+                    }?.addOnFailureListener {
+
+                    }
+                }.addOnFailureListener { excpetion ->
+                    exibirMensagem("Erro ao fazer o upload da imagem")
+                }
+        }
+    }
+
+    private fun atualizarDadosPerfil(idUsuario: String, dados: Map<String, String>) {
+        firestore
+            .collection("usuarios")
+            .document(idUsuario)
+            .update(dados)
+            .addOnSuccessListener {
+                exibirMensagem("Sucesso ao atualizar o perfil")
+            }.addOnFailureListener { exception ->
+                exibirMensagem("Erro ao atualizar o perfil")
+            }
+    }
+
+    private fun inicializarEventosClique() {
+        binding.fabSelecionar.setOnClickListener {
+            if (temPermissaoGaleria) {
+                gerenciadorGaleria.launch("image/*")
+            } else {
+                exibirMensagem("Não tem permissão para acessar a galeria")
+                solicitarPermissoes()
+            }
+        }
+
+        binding.btnAtualizarPerfil.setOnClickListener {
+            val nomeUsuario = binding.editNomePerfil.text.toString()
+
+            if (nomeUsuario.isNotEmpty()) {
+                val idUsuario = firebaseAuth.currentUser?.uid
+
+                if (idUsuario != null) {
+                    val dados = mapOf(
+                        "nome" to nomeUsuario
+                    )
+                    atualizarDadosPerfil(idUsuario, dados)
+                }
+            } else {
+                exibirMensagem("Preencha o nome para atualizar")
+            }
+        }
+    }
+
     private fun solicitarPermissoes() {
         //Verificar se o usuário já tem a permissão
         temPermissaoCamera = ContextCompat.checkSelfPermission(
