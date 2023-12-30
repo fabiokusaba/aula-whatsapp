bd11bb1242590384d6cb263557b8767b88c0473c
Fabio Kusaba <fabinhokb44@gmail.com>
2023-12-29 21:13:41 -0300

    commit "adicionado lib Picasso"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     //Cloud Storage
     implementation("com.google.firebase:firebase-storage-ktx")
 
+    //Picasso
+    implementation("com.squareup.picasso:picasso:2.8")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
