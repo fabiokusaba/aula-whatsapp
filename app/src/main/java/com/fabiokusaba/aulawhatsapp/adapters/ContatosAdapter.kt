e15100309a5e25e2082955f41f0d37dad1a42de2
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-01 08:27:14 -0300

    commit "adicionado exibição de contatos"

---
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/adapters/ContatosAdapter.kt
@@ -0,0 +1,46 @@
+package com.fabiokusaba.aulawhatsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.fabiokusaba.aulawhatsapp.databinding.ItemContatosBinding
+import com.fabiokusaba.aulawhatsapp.model.Usuario
+import com.squareup.picasso.Picasso
+
+class ContatosAdapter : Adapter<ContatosAdapter.ContatosViewHolder>() {
+    private var listaContatos = emptyList<Usuario>()
+    fun adicionarLista(lista: List<Usuario>) {
+        listaContatos = lista
+        notifyDataSetChanged()
+    }
+
+    inner class ContatosViewHolder(
+        private val binding: ItemContatosBinding
+    ): ViewHolder(binding.root) {
+        fun bind(usuario: Usuario) {
+            binding.textContatoNome.text = usuario.nome
+            Picasso.get()
+                .load(usuario.foto)
+                .into(binding.imageContatoFoto)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContatosViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = ItemContatosBinding.inflate(
+            inflater, parent, false
+        )
+
+        return ContatosViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return listaContatos.size
+    }
+
+    override fun onBindViewHolder(holder: ContatosViewHolder, position: Int) {
+        val usuario = listaContatos[position]
+        holder.bind(usuario)
+    }
+}
--- app/src/main/java/com/fabiokusaba/aulawhatsapp/fragments/ContatosFragment.kt
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/fragments/ContatosFragment.kt
@@ -5,15 +5,81 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.fabiokusaba.aulawhatsapp.R
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fabiokusaba.aulawhatsapp.adapters.ContatosAdapter
+import com.fabiokusaba.aulawhatsapp.databinding.FragmentContatosBinding
+import com.fabiokusaba.aulawhatsapp.model.Usuario
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 
 class ContatosFragment : Fragment() {
+    private lateinit var binding: FragmentContatosBinding
+    private lateinit var eventoSnapshot: ListenerRegistration
+    private lateinit var contatosAdapter: ContatosAdapter
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false)
+        binding = FragmentContatosBinding.inflate(
+            inflater, container, false
+        )
+
+        contatosAdapter = ContatosAdapter()
+        binding.rvContatos.adapter = contatosAdapter
+        binding.rvContatos.layoutManager = LinearLayoutManager(context)
+        binding.rvContatos.addItemDecoration(
+            DividerItemDecoration(
+                context, LinearLayoutManager.VERTICAL
+            )
+        )
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adicionarListenerContatos()
+    }
+
+    private fun adicionarListenerContatos() {
+        eventoSnapshot = firestore
+            .collection("usuarios")
+            .addSnapshotListener{ querySnapshot, erro ->
+                val listaContatos = mutableListOf<Usuario>()
+                val documentos = querySnapshot?.documents
+
+                documentos?.forEach { documentSnapshot ->
+                    val idUsuarioLogado = firebaseAuth.currentUser?.uid
+                    val usuario = documentSnapshot.toObject(Usuario::class.java)
+
+                    if (usuario != null && idUsuarioLogado != null) {
+                        if (idUsuarioLogado != usuario.id) {
+                            listaContatos.add(usuario)
+                        }
+                    }
+                }
+
+                //Lista de contatos (atualizar o RecyclerView)
+                if (listaContatos.isNotEmpty()) {
+                    contatosAdapter.adicionarLista(listaContatos)
+                }
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        eventoSnapshot.remove()
     }
 }
