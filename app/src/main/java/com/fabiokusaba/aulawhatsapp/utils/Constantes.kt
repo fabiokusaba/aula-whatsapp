f0925361d3406c5bec69cde3380670f06adba200
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-01 09:45:25 -0300

    commit "adicionado constante BD_MENSAGENS"

--- app/src/main/java/com/fabiokusaba/aulawhatsapp/utils/Constantes.kt
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/utils/Constantes.kt
@@ -3,4 +3,6 @@ package com.fabiokusaba.aulawhatsapp.utils
 object Constantes {
     const val ORIGEM_CONTATO = "origem_contato"
     const val ORIGEM_CONVERSA = "origem_conversa"
+
+    const val BD_MENSAGENS = "mensagens"
 }
