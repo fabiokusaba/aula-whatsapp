bc9b357db823c81df65e75fd0dde2e9703a245c2
Fabio Kusaba <fabinhokb44@gmail.com>
2024-01-01 10:02:38 -0300

    commit "adicionado salvar mensagens Firestore"

--- app/src/main/java/com/fabiokusaba/aulawhatsapp/activities/MensagensActivity.kt
+++ app/src/main/java/com/fabiokusaba/aulawhatsapp/activities/MensagensActivity.kt
@@ -4,13 +4,25 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fabiokusaba.aulawhatsapp.databinding.ActivityMensagensBinding
+import com.fabiokusaba.aulawhatsapp.model.Mensagem
 import com.fabiokusaba.aulawhatsapp.model.Usuario
 import com.fabiokusaba.aulawhatsapp.utils.Constantes
+import com.fabiokusaba.aulawhatsapp.utils.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 
 class MensagensActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMensagensBinding.inflate(layoutInflater)
+    }
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
     }
 
     private var dadosDestinatario: Usuario? = null
@@ -21,6 +33,52 @@ class MensagensActivity : AppCompatActivity() {
 
         recuperarDadosUsuarioDestinatario()
         inicializarToolbar()
+        inicializarEventoClique()
+    }
+
+    private fun inicializarEventoClique() {
+        binding.fabEnviar.setOnClickListener {
+            val mensagem = binding.editMensagem.text.toString()
+            salvarMensagem(mensagem)
+        }
+    }
+
+    private fun salvarMensagem(textoMensagem: String) {
+        if (textoMensagem.isNotEmpty()) {
+            val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+            val idUsuarioDestinatario = dadosDestinatario?.id
+
+            if (idUsuarioRemetente != null && idUsuarioDestinatario != null) {
+                val mensagem = Mensagem(
+                    idUsuarioRemetente, textoMensagem
+                )
+
+                //Salvar para o remetente
+                salvarMensagemFirestore(
+                    idUsuarioRemetente, idUsuarioDestinatario, mensagem
+                )
+
+                //Salvar mensagem para o destinatario
+                salvarMensagemFirestore(
+                    idUsuarioDestinatario, idUsuarioRemetente, mensagem
+                )
+
+                binding.editMensagem.setText("")
+            }
+        }
+    }
+
+    private fun salvarMensagemFirestore(
+        idUsuarioRemetente: String, idUsuarioDestinatario: String, mensagem: Mensagem
+    ) {
+        firestore
+            .collection(Constantes.BD_MENSAGENS)
+            .document(idUsuarioRemetente) //Usuário logado -> aquele que está enviando a msg
+            .collection(idUsuarioDestinatario)
+            .add(mensagem)
+            .addOnFailureListener {
+                exibirMensagem("Erro ao enviar mensagem")
+            }
     }
 
     private fun inicializarToolbar() {
